aab0a4ff237b5dc4fa1be7fc2d50f12849f242bb
Ta Huu Tu <tahuutu03@gmail.com>
2024-05-08 18:16:31 +0700

    my project

---
+++ PanelControl.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class PanelControl extends JPanel {
+	public static JLabel jLabel;
+
+	public JLabel jLabel2;
+	public static Font font = new Font("TimeNewRoman", Font.BOLD, 15);
+//	public static Border border = BorderFactory.createLineBorder(Color.BLACK, 5);
+
+	public PanelControl() {
+		jLabel2 = new JLabel("~~~~~LƯỢT CHƠI HIỆN TẠI~~~~~");
+		jLabel = new JLabel("Đội xanh dương đi trước!!!");
+		jLabel.setFont(font);
+		jLabel.setForeground(Color.blue);
+		setPreferredSize(new Dimension(200, 600));
+		setLayout(new BorderLayout());
+		setBackground(Color.LIGHT_GRAY);
+		add(jLabel2, BorderLayout.NORTH);
+		add(jLabel, BorderLayout.CENTER);
+		JPanel jPanel = new JPanel();
+
+		JButton restart = new Restart();
+
+		JButton exit = new Exit();
+
+		jPanel.setLayout(new GridLayout(2, 1));
+		jPanel.add(restart);
+		jPanel.add(exit);
+		jPanel.setBackground(Color.LIGHT_GRAY);
+		add(jPanel, BorderLayout.SOUTH);
+
+	}
+
+	public static void update() {
+		jLabel.setHorizontalAlignment(SwingConstants.CENTER); // Đặt căn giữa ngang
+		jLabel.setVerticalAlignment(SwingConstants.CENTER);
+		if (GameManager.currentPlayer == 0) {
+			jLabel.setText("Đến lượt đội xanh dương!");
+			jLabel.setFont(font);
+			jLabel.setForeground(Color.blue);
+
+		}
+		if (GameManager.currentPlayer == 1) {
+			jLabel.setText("Đến lượt đội đỏ!");
+			jLabel.setFont(font);
+			jLabel.setForeground(Color.red);
+
+		}
+		if (GameManager.currentPlayer == 2) {
+			jLabel.setText("Đến lượt đội xanh lá!");
+			jLabel.setFont(font);
+			jLabel.setForeground(Color.green);
+
+		}
+		if (GameManager.currentPlayer == 3) {
+			jLabel.setText("Đến lượt đội vàng!");
+			jLabel.setFont(font);
+			jLabel.setForeground(Color.yellow);
+//			border = BorderFactory.createLineBorder(Color.yellow, 5);
+		}
+
+	}
+}
